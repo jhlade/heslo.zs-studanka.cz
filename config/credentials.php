91c56f63fa71bfb936fd1e44b716746df64c2604
jhlade <jan.hladena@gmail.com>
2023-09-03 12:01:04 +0200

    Update credentials.php

--- config/credentials.php
+++ config/credentials.php
@@ -1,5 +1,5 @@
 <?php
 	// authorized non-interactive service credentials
 	// this service has permission to read and write user attributes in specified OU(s)
-	$svcupn = "service@zsstu.local";
-	$svcpwd = "Pa$$.w0rd";
+	$svcupn = "service@domain.tld";
+	$svcpwd = "<service password>";
